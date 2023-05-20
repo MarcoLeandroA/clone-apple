13e5811e488abb7061acbe7f3c1ee728f48377a4
Marco <marcoaranibar12@gmail.com>
2023-05-20 16:36:39 -0500

    restart

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
 export function Header() {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
   return (
     <header className='header'>
       <div className="logo">
@@ -14,19 +8,17 @@ export function Header() {
         <div className="logo-menu">
           <ion-icon name="menu-outline" onClick={handleClick}></ion-icon>
         </div>
-        {isActive && (
-          <ul className="nav-select">
-            <li><a href="">Mac</a></li>
-            <li><a href="">iPad</a></li>
-            <li><a href="">iPhone</a></li>
-            <li><a href="">Watch</a></li>
-            <li><a href="">AirPods</a></li>
-            <li><a href="">TV y Casa</a></li>
-            <li><a href="">Entretenimiento</a></li>
-            <li><a href="">Soporte</a></li>
-            <li><a href="">Donde comprar</a></li>
-          </ul>
-        )}
+        <ul className="nav-select">
+          <li><a href="">Mac</a></li>
+          <li><a href="">iPad</a></li>
+          <li><a href="">iPhone</a></li>
+          <li><a href="">Watch</a></li>
+          <li><a href="">AirPods</a></li>
+          <li><a href="">TV y Casa</a></li>
+          <li><a href="">Entretenimiento</a></li>
+          <li><a href="">Soporte</a></li>
+          <li><a href="">Donde comprar</a></li>
+        </ul>
       </nav>
     </header>
   );
