93e78cf3416c7867ee71199ca3be9d04c14313c9
Marco <marcoaranibar12@gmail.com>
2023-05-21 22:09:02 -0500

    end version 1.0.0

--- src/components/present.jsx
+++ src/components/present.jsx
@@ -3,7 +3,15 @@ export function Present(props) {
     <div className="present-content">
       <h2>{props.title}</h2>
       <p>{props.expression}</p>
-      <a href="#">Más información <ion-icon name="chevron-forward-outline"></ion-icon></a>
+      <span className="more-info">
+        <a href="#">Más información<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l14 0"></path>
+          <path d="M13 18l6 -6"></path>
+          <path d="M13 6l6 6"></path>
+        </svg></a>
+
+      </span>
       <img src={`./images/iphone-${props.image}.png`} alt={props.descripttion} />
     </div>
   );
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -7,7 +7,14 @@ export function Slider() {
         para ti.
       </span>
       <h3>Cada producto está diseñado para ser accesible.</h3>
-      <a href="#">Mas información<ion-icon name="chevron-forward-outline"></ion-icon></a>
+      <span className='more-info'>
+        <a href="#">Mas información<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l14 0"></path>
+          <path d="M13 18l6 -6"></path>
+          <path d="M13 6l6 6"></path>
+        </svg></a>
+      </span>
     </div>
   );
 }
